079dad44b5381329632af2a5a610a37cadded235
Patrick Starliper <48708250+PatrickStarliper@users.noreply.github.com>
2019-04-23 13:19:36 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
-// chrispapi in the house
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
 
 int main(){
-
-
+  string difficulty;
+  int check = 0;
+  while(check == 0){
+    cout << "Please enter your difficulty: (easy/medium/hard) "
+    cin >> difficulty;
+    if (difficulty == "easy" || difficulty == "medium" || difficulty == "hard"){
+      check = 1;
+    }
+    else{
+      cout << "Incorrect input.";
+      cout << "Please enter your difficulty: (easy/medium/hard) ";
+      cin >> difficulty;
+    }
+  }
+  
 
   return 0;
 }
