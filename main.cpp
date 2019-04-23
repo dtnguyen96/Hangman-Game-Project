b29aaf66aa73c9b36424adb41d071ad008ff32bd
Patrick Starliper <48708250+PatrickStarliper@users.noreply.github.com>
2019-04-23 14:16:52 -0500

    random

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <random>
 using namespace std;
 
 int main(){
@@ -9,7 +10,7 @@ int main(){
   string difficulty;
   int check = 0;
   while(check == 0){
-    cout << "Please enter your difficulty: (easy/medium/hard) "
+    cout << "Please enter your difficulty: (easy/medium/hard) ";
     cin >> difficulty;
     if (difficulty == "easy" || difficulty == "medium" || difficulty == "hard"){
       check = 1;
@@ -21,10 +22,22 @@ int main(){
     }
   }
 
-  // select a random word of the same level
-  ifstream inFile("dictionary.txt");
-  int random = rand() % 10
-
+  // select a random word of the selected level
+  int num_words = 10;
+  string array[10] = {};
+  ifstream inFile("dictionary_easy.txt");
+  if (!inFile.is_open()){
+    cout << "File not open." << endl;
+  }
+  int random = rand() % 10;
+  cout << random << endl;
+  string line;
+  int counter = 0;
+  while(getline(inFile,line)){
+    array[counter] = line;
+    counter++;
+  }
+  cout << "random word: " << array[random];
 
   return 0;
 }
