04c21a8c01a8f7c64b256ecc53cea19817e122fa
neriyahbutler <neriyah.butler21@gmail.com>
2019-04-23 14:17:36 -0500

    nice

---
+++ difficulty.h
@@ -0,0 +1,23 @@
+class difficuly {
+public:
+  difficuly(level="") : level(level){}
+
+
+  void setMaxGuesses(){
+    if(level = "easy")
+  }
+  int getMaxGuesses(){ return maxGuesses;}
+
+  void checkGameStatus(string status){
+    if(string == "win"){
+      game_win = true;
+    }else{
+      game_win = false;
+    }
+  }
+private:
+  int maxGuesses;
+  bool game_win;
+protected:
+  string level;
+};
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 // chrispapi in the house
+#include <iostream>
+#incude <fstream>
+using namespace std;
 
 int main(){
-
 
 
   return 0;
